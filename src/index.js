97a89396885490a1ddffc0fdace3d2e07a25acda
Raghuram <raghu_13_sa@yahoo.com>
2025-06-07 16:20:50 +0800

    Made UI changes

--- src/index.js
+++ src/index.js
@@ -15,7 +15,12 @@ import { createRoot } from 'react-dom/client';
   document.getElementById('root')
 );*/
 const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
