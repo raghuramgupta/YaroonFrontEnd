7e64b1f2be1f2d3e59191016bb00140cb1d0e376
Raghuram <raghu_13_sa@yahoo.com>
2025-06-07 13:56:14 +0800

    Made UI changes

--- src/index.js
+++ src/index.js
@@ -5,16 +5,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Replace your existing ReactDOM.render with:
 ReactDOM.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
-    <App />
-  </BrowserRouter>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
   document.getElementById('root')
 );
 // If you want to start measuring performance in your app, pass a function
